ab8b9522bef4ef7ee60cc6adb4b71490fa6c45dc
Pucaet <yang.z.1879@gmail.com>
2020-10-08 09:04:43 -0500

    XP logging now has its own loop

--- cog/xptracker.py
+++ cog/xptracker.py
@@ -12,6 +12,7 @@ from cog.datamanager import DataManager
 
 
 XP_UPDATE_INTERVAL = 6
+XP_LOG_INTERVAL = 5  # in minutes
 
 
 @DataManager.register("_prevTXp")
@@ -41,24 +42,30 @@ class XPTracker(commands.Cog):
             if memberData["name"] in trackedIgns:
                 id_ = self._memberManager.ignIdMap[ memberData["name"]]
                 newTXp = memberData["contributed"]
-                dxp = self._lb.set_stat(id_, newTXp)
-
-                if dxp > 0:
-                    stat = self._lb.get_stat(id_)
-                    prev = self._lastLoggedVal.get(id_, -1)
-                    if stat == prev:
-                        return
-                    text = "  |  ".join([
-                        f"**{memberData['name']}** (+{dxp})",
-                        f"__Total__ -> {stat:,}",
-                        f"__Acc__ -> {self._lb.get_acc(id_):,}",
-                        f"__BW__ -> {self._lb.get_bw(id_):,}"])
-                    await self._config.send("xpLog", text)
-                    self._lastLoggedVal[id_] = stat
+                self._lb.set_stat(id_, newTXp)
     
     @_update.before_loop
     async def _before_update(self):
         Logger.bot.debug("Starting xp tracking loop")
+    
+    @tasks.loop(minutes=XP_LOG_INTERVAL)
+    async def _xp_log(self):
+        for id_ in self._memberManager.members:
+            stat = self._lb.get_stat(id_)
+            prev = self._lastLoggedVal.get(id_, -1)
+            if stat == prev:
+                return
+            text = "  |  ".join([
+                f"**{self._memberManager.members[id_].ign}** (+{dxp})",
+                f"__Total__ -> {stat:,}",
+                f"__Acc__ -> {self._lb.get_acc(id_):,}",
+                f"__BW__ -> {self._lb.get_bw(id_):,}"])
+            await self._config.send("xpLog", text)
+            self._lastLoggedVal[id_] = stat
+    
+    @_xp_log.before_loop
+    async def _before_xp_log(self):
+        Logger.bot.debug("Starting xp logging loop")
     
     @parser("xp", ["acc"], ["bw"], isGroup=True)
     async def display_xp_lb(self, ctx: commands.Context, acc, bw):
