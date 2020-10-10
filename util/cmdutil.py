7df91ab6026165be891ac808fb7601a58fcc535c
Pucaet <yang.z.1879@gmail.com>
2020-10-10 11:49:29 -0500

    parser now support custom dest arg name

--- util/cmdutil.py
+++ util/cmdutil.py
@@ -17,7 +17,7 @@ class CatchableArgumentParser(ArgumentParser):
 "-arg"                      store argument
 ["arg", ("choice", ...)]    choice argument
 """
-def parser(name, *args, isGroup=False, parent=None):
+def parser(name, *args, isGroup=False, parent=None, **destMap):
     argParser = CatchableArgumentParser(prog=f"]{name}", add_help=False)
     for arg in args:
         argType = type(arg)
@@ -25,20 +25,22 @@ def parser(name, *args, isGroup=False, parent=None):
             #  "arg..."
             if arg.endswith("..."):
                 arg = arg[:-3]
-                argParser.add_argument(dest=arg, action="extend", nargs="+", type=str)
+                argParser.add_argument(
+                    dest=destMap.pop(arg, arg), action="extend", nargs="+", type=str)
             #  "-arg"
             elif arg[0] == "-":
                 arg = arg[1:]
-                argParser.add_argument(f"-{arg[0]}", f"--{arg}", action="store", dest=arg)
+                argParser.add_argument(
+                    f"-{arg[0]}", f"--{arg}", action="store", dest=destMap.pop(arg, arg))
             #  "arg"
             else:
-                argParser.add_argument(dest=arg)
+                argParser.add_argument(dest=destMap.pop(arg, arg))
         elif argType == list:
             #  ["arg"]
             if len(arg) == 1:
                 arg = arg[0]
-                argParser.add_argument(
-                    f"-{arg[0]}", f"--{arg}", action="store_true", dest=arg)
+                argParser.add_argument(f"-{arg[0]}", f"--{arg}", 
+                                       action="store_true", dest=destMap.pop(arg, arg))
             #  ["arg", ("choice", ...)]
             elif len(arg) == 2:
                 [arg, choices] = arg
@@ -51,10 +53,12 @@ def parser(name, *args, isGroup=False, parent=None):
         @cmdWrapper
         async def parsable(self, ctx, *arguments):
             try:
-                args = argParser.parse_args(arguments)
+                args = vars(argParser.parse_args(arguments))
             except Exception as e:
                 await ctx.send(embed=make_alert(":".join(str(e).split(":")[2:])))
                 return
-            await func(self, ctx, **vars(argParser.parse_args(arguments)))
+            for cmdArgName, funcArgName in destMap.items():
+                args[funcArgName] = args.pop(cmdArgName, None)
+            await func(self, ctx, **args)
         return parsable
     return wrapper
