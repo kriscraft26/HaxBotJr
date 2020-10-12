677512afdd2527cedbbf99e9d27d12daf6afb372
Pucaet <yang.z.1879@gmail.com>
2020-10-11 21:22:10 -0500

    Config data now stored as id

--- cog/configuration.py
+++ cog/configuration.py
@@ -17,8 +17,8 @@ from cog.datamanager import DataManager
 class Configuration(commands.Cog):
 
     DEFAULT_CONFIG = {
-        "user.dev": {},
-        "user.ignore": {},
+        "user.dev": set(),
+        "user.ignore": set(),
         "role.visual": {},
         "role.personal": {},
         "channel.xpLog": None,
@@ -27,10 +27,6 @@ class Configuration(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self._config = Configuration.DEFAULT_CONFIG
-        self._channels = {
-            "xpLog": None,
-            "bwReport": None,
-        }
         self._groups = {
             "guild": ["MoonWalker", "Cosmonaut", "Rocketeer", "Space Pilot", 
                       "Engineer", "Cadet"],
@@ -49,26 +45,33 @@ class Configuration(commands.Cog):
         for key in removedKeys:
             del self._config[key]
         
-        for key in self._channels:
-            channelId = self._config["channel." + key]
-            if channelId:
-                self._channels[key] = self.guild.get_channel(channelId)
+        sample = self._config["user.dev"].pop()
+        self._config["user.dev"].add(sample)
+        if type(sample) == str:
+            Logger.bot.debug("outed config data detected, updating...")
+
+            getMId = lambda s: self.parse_user(s).id
+            getRId = lambda r: self.parse_role(r).id
+
+            self._config["user.dev"] = set(map(getMId, self._config["user.dev"]))
+            self._config["user.ignore"] = set(map(getMId, self._config["user.ignore"]))
+
+            self._config["role.visual"] = {getRId(r): set(map(getRId, rs)) 
+                for r, rs in self._config["role.visual"].items()}
+            self._config["role.personal"] = {getRId(r): set(map(getMId, ms)) 
+                for r, ms in self._config["role.personal"].items()}
+            
+            Logger.bot.debug(f"-> {self._config}")
 
     def __call__(self, configName: str):
-        if configName.startswith("channel."):
-            return self._channels[configName.split(".")[1]]
         return self._config[configName]
     
     def _set(self, name: str, val):
         Logger.bot.info(f"Config {name}: {self._config[name]} -> {val}")
         self._config[name] = val
-
-        configSub = name.split(".")[1]
-        if configSub in self._channels:
-            self._channels[configSub] = self.guild.get_channel(val) if val else None
     
     async def send(self, channelName, text, **kwargs):
-        channel = self._channels.get(channelName, None)
+        channel = self.bot.get_channel(self._config["channel." + channelName])
         if not channel:
             return
         await channel.send(text, **kwargs)
@@ -81,7 +84,7 @@ class Configuration(commands.Cog):
         return roleRank in self._groups[groupName]
     
     def is_of_user(self, userName: str, member: Member) -> bool:
-        return str(member) in self._config[f"user.{userName}"]
+        return member.id in self._config[f"user.{userName}"]
 
     def get_rank(self, member: Member) -> Union[None, Tuple[str, str]]:
         if self.is_of_user("ignore", member):
@@ -91,26 +94,28 @@ class Configuration(commands.Cog):
         if not nick or " " not in nick:
             return None
 
-        memberRoles = list(map(lambda r: r.name, member.roles))
-        roleRank = find(lambda r: r in memberRoles, self._groups["guild"])
+        roleRank = find(lambda r: r.name in self._groups["guild"], member.roles)
         if not roleRank:
             return None
 
         [*nameRank, _] = nick.split(" ")
         nameRank = " ".join(nameRank).strip()
+        nameRank = find(lambda r: r.name == nameRank, member.roles)
+        if not nameRank:
+            Logger.bot.warning(f"{nick}: missing nick rank")
 
-        if roleRank == nameRank:
-            return (roleRank, None)
+        if roleRank.id == nameRank.id:
+            return (roleRank.name, None)
         
-        if nameRank in self._config["role.visual"]:
-            if roleRank in self._config["role.visual"][nameRank]:
-                return (roleRank, nameRank)
+        if nameRank.id in self._config["role.visual"]:
+            if roleRank.id in self._config["role.visual"][nameRank.id]:
+                return (roleRank.name, nameRank.name)
             Logger.bot.warning(f"{nick}: visual role mismatch")
             return None
         
-        if nameRank in self._config["role.personal"]:
-            if str(member) in self._config["role.personal"][nameRank]:
-                return (roleRank, nameRank)
+        if nameRank.id in self._config["role.personal"]:
+            if member.id in self._config["role.personal"][nameRank.id]:
+                return (roleRank.name, nameRank.name)
             Logger.bot.warning(f"{nick}: personal role mismatch")
             return None
         
@@ -127,8 +132,11 @@ class Configuration(commands.Cog):
         checker = self.is_of_group if type_ == "group" else self.is_of_user
         passed = checker(name, ctx.author)
         if not passed:
-            names = ", ".join(
-                self._config[fieldName] if type_ == "user" else self._groups[name])
+            if type_ == "user":
+                names = map(lambda i: str(self.guild.get_member(i)), self._config[fieldName])
+            else:
+                names = self._groups[name]
+            names = ", ".join(names)
             alert = make_alert("You have no permission to use this command",
                 subtext=f"only {names} can use it")
             await ctx.send(embed=alert)
@@ -139,43 +147,8 @@ class Configuration(commands.Cog):
 
     @parser("config", isGroup=True)
     async def display_config(self, ctx: commands.Context):
-        entries = self._config.keys()
-        fmt = lambda k: f"{k}\n{self(k)}\n"
-        entries = list(map(fmt, entries))
-        entries[-1] = entries[-1].strip()
-
-        pages = make_entry_pages(entries, maxEntries=5, title="Configuration")
-        await PagedMessage(pages, ctx.channel).init()
-    
-    async def _set_channel(self, ctx: commands.Context, channel, channelType):
-        channel: TextChannel = self.parse_channel(channel)
-        if not channel:
-            await ctx.send(embed=make_alert("bad channel input"))
-            return
-        channelName = f"#{channel.name}"
-
-        if self._config[f"channel.{channelType}"] == channel.id:
-            text = f"{channelName} is already set as {channelType} channel."
-            await ctx.send(embed=make_alert(text, color=COLOR_INFO))
-            return
-
-        text = f"Are you sure to set {channelName} as {channelType} channel?"
-        successText = f"Successfully set {channelName} as {channelType} channel."
-
-        cb = lambda m: self._set(f"channel.{channelType}", channel.id)
-        await ConfirmMessage(ctx, text, successText, cb).init()
-    
-    async def _reset_channel(self, ctx: commands.Context, channelType):
-        channel = self._channels[channelType]
-        if not channel:
-            text = f"There are no channel set as {channelType} channel"
-            await ctx.send(embed=make_alert(text, color=COLOR_INFO))
-            return
-        text = f"Are you sure to reset {channelType} channel? (currently #{channel.name})"
-        successText = f"Successfully reset {channelType} channel."
-
-        cb = lambda m: self._set(f"channel.{channelType}", None)
-        await ConfirmMessage(ctx, text, successText, cb).init()
+        text = "\n".join(self._config.keys())
+        await ctx.send(embed=make_alert(text, title="Configuration", color=COLOR_INFO))
     
     def parse_channel(self, target: str):
         m = match("<#([0-9]+)>", target)
@@ -201,15 +174,44 @@ class Configuration(commands.Cog):
     @parser("config channel", ["type", ("xpLog", "bwReport")], ["reset"], "-set",
         parent=display_config, type="type_", set="set_")
     async def config_channel(self, ctx: commands.Context, type_, reset, set_):
-        if reset:
-            await self._reset_channel(ctx, type_)
-        elif set_:
-            await self._set_channel(ctx, set_, type_)
-        else:
+        field = "channel." + type_
+
+        if not reset and not set_:
             title = "channel." + type_
-            channel = self._channels[type_]
+            channel = self.guild.get_channel(self._config[field])
             text = "#" + channel.name if channel else "Not set"
             await ctx.send(embed=make_alert(text, title=title, color=COLOR_INFO))
+            return
+
+        if reset:
+            channel = self.guild.get_channel(self._config[field])
+            if not channel:
+                text = f"There are no channel set as {type_} channel"
+                await ctx.send(embed=make_alert(text, color=COLOR_INFO))
+                return
+            text = f"Are you sure to reset {type_} channel? (currently #{channel.name})"
+            successText = f"Successfully reset {type_} channel."
+
+            cb = lambda m: self._set(f"channel.{type_}", None)
+        else:
+            channel: TextChannel = self.parse_channel(set_)
+            if not channel:
+                await ctx.send(embed=make_alert("bad channel input"))
+                return
+            channelName = f"#{channel.name}"
+
+            if self._config[field] == channel.id:
+                text = f"{channelName} is already set as {type_} channel."
+                await ctx.send(embed=make_alert(text, color=COLOR_INFO))
+                return
+
+            text = f"Are you sure to set {channelName} as {type_} channel?"
+            successText = f"Successfully set {channelName} as {type_} channel."
+
+            cb = lambda m: self._set(field, channel.id)
+        
+        await ConfirmMessage(ctx, text, successText, cb).init()
+            
     
     @parser("config user", ["type", ("dev", "ignore")], "-remove", "-add",
         parent=display_config, type="type_")
@@ -218,27 +220,29 @@ class Configuration(commands.Cog):
         users = self._config[field]
 
         if not remove and not add:
-            text = ", ".join(users) if users else "Empty"
+            if users:
+                text = ", ".join(map(lambda i: str(self.guild.get_member(i)), users))
+            else:
+                text = "Empty"
             await ctx.send(embed=make_alert(text, title=field, color=COLOR_INFO))
+            return
+        
+        user = self.parse_user(remove or add)
+        if not user:
+            await ctx.send(embed=make_alert("bad user input"))
             return
 
         if remove:
-            if remove not in users:
-                text = f"{remove} is not part of {type_} users."
+            if user.id not in users:
+                text = f"{user} is not part of {type_} users."
                 await ctx.send(embed=make_alert(text, color=COLOR_INFO))
                 return
             
-            text = f"Are you sure to remove {remove} from {type_} users?"
-            successText = f"Successfully removed {remove} from {type_} users."
-            cb = lambda m: self._set(field, users.difference({remove}))
+            text = f"Are you sure to remove {user} from {type_} users?"
+            successText = f"Successfully removed {user} from {type_} users."
+            cb = lambda m: self._set(field, users.difference({user.id}))
         else:
-            user = self.parse_user(remove or add)
-            if not user:
-                await ctx.send(embed=make_alert("bad user input"))
-                return
-            user = str(user)
-
-            if user in users:
+            if user.id in users:
                 text = f"{user} is already part of {type_} users."
                 await ctx.send(embed=make_alert(text, color=COLOR_INFO))
                 return
@@ -246,7 +250,7 @@ class Configuration(commands.Cog):
             text = f"Are you sure to add {user} to {type_} users?"
             successText = f"Successfully added {user} to {type_} users."
 
-            cb = lambda m: self._set(field, users.union({user}))
+            cb = lambda m: self._set(field, users.union({user.id}))
         
         await ConfirmMessage(ctx, text, successText, cb).init()
 
