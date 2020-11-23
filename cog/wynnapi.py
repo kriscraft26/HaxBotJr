33a93102554fd61321af3a601191466ef496cfbf
Pucaet <yang.z.1879@gmail.com>
2020-11-22 19:38:22 -0600

    added `]api`

--- cog/wynnapi.py
+++ cog/wynnapi.py
@@ -8,7 +8,10 @@ from discord.ext import tasks, commands
 
 import wynnapi
 from logger import Logger
+from msgmaker import decorate_text
+from state.apistamp import APIStamp
 from util.timeutil import now
+from util.cmdutil import parser
 
 
 UPDATE_INTERVAL = 10  # Number of seconds between each update() call.
@@ -29,3 +32,14 @@ class WynnAPI(commands.Cog):
     async def _before_update(self):
         await self.bot.wait_until_ready()
         Logger.bot.debug("Starting Wynncraft API update loop")
+    
+    @parser("api")
+    async def display_api_status(self, ctx: commands.Context):
+        text = ""
+        curr = now()
+        endpoints = ("guildStats", "serverList", "terrList")
+        for ep in endpoints:
+            params = getattr(wynnapi.WynnAPI, ep).params
+            updateTime = APIStamp.stamps[id(params)][1]
+            text += "%-13s  %2ds\n" % (params["action"], (curr - updateTime).total_seconds())
+        await ctx.send(decorate_text(text, title="API Update Status"))
